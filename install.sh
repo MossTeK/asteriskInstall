86cd4243e73bfbcb287207093d884748277305ee
MossTeK <moss.personal.email@gmail.com>
2023-12-07 19:53:53 +0000

    updating dev branch to be in sync with what im testing on vscode

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ touch installLog.txt
 installLog="$(dirname "$(readlink -f "$0")")/installLog.txt"
 
 # Check if script is running as root
-if [ "$(id -u)" -ne 0 ]; then
+if ! id -u 0; then
     echo "This script must be run as root. Exiting..." | tee $installLog
     exit 1
 fi
@@ -14,9 +14,9 @@ function main() {
     for arg in "$@"; do
         $arg | tee $installLog
         if $? >> $installLog; then
-           echo "ran $arg"
+           echo "ran $arg" | tee $installLog
         else
-            echo "unable to run $arg"
+           echo "unable to run $arg" | tee $installLog
         fi
     done
 
@@ -50,8 +50,9 @@ function installAsterisk() {
     #move into asterisk directory, run install_prereq & ./configure. Declare asterisk modules
     cd /usr/src/asterisk-18.*/
     contrib/scripts/install_prereq install 
-    menuselect/menuselect --enable app_dial --enable app_stack --enable app_voicemail --enable chan_bridge --enable chan_sip --enable codec_alaw --enable codec_dahdi --enable codec_ulaw --enable res_adsi --enable res_rtp_asterisk --enable app_authenticate --enable app_cdr --enable app_channelredirect --enable app_chanspy --enable app_controlplayback --enable app_dictate --enable app_directed_pickup --enable app_directory --enable app_disa --enable app_dumpchan --enable app_fax --enable app_externalivr --enable app_exec --enable app_echo --enable app_flash --enable app_followme --enable app_originate --enable app_mixmonitor --enable app_milliwatt --enable app_jack --enable app_meetme --enable app_page --enable app_plaback --enable app_playtones --enable app_read --enable app_readexten --enable app_record --enable app_sayunixtime --enable app_senddtmf --enable app_skel --enable app_speech_utils --enable app_softhangup --enable app_talkdetect --enable app_system --enable app_transfer --enable app_userevent --enable app_verbose --enable app_waitforsilence --enable app_waituntil --enable app_while --enable cdr_adaptive_odbc --enable cdr_manager --enable cdr_custom --enable cdr_pgsql --enable cdr_syslog --enable cel_pgsql --enable cel_odbc --enable cel_manager --enable cel_custom --enable cel_sqlite3_custom --enable chan_agent --enable chan_alsa --enable chan_oss --enable chan_multicast_rtp --enable chan_local --enable chan_iax2 --enable chan_dahdi --enable codec_a_mu --enable codec_g722 --enable codec_gsm --enable format_g729 --enable format_vox --enable format_wav --enable format_siren14 --enable func_blacklist --enable func_audiohookinherit --enable func_aes --enable func_cdr --enable func_callerid --enable func_aes --enable func_iconv --enable func_global --enable func_extstate --enable func_env --enable func_enum --enable func_dialplan --enable func_dialgroup --enable pbx_dundi --enable pbx_config --enable pbx_lua --enable pbx_realtime --enable pbx_spool --enable res_config_mysql menuselect.makeopts
-    ./configure 
+    ./configure
+    make menuselect.makeopts
+    menuselect/menuselect --enable app_dial --enable app_stack --enable app_voicemail --enable chan_sip --enable codec_alaw --enable codec_dahdi --enable codec_ulaw --enable res_adsi --enable res_rtp_asterisk --enable app_authenticate --enable app_cdr --enable app_channelredirect --enable app_chanspy --enable app_controlplayback --enable app_dictate --enable app_directed_pickup --enable app_directory --enable app_disa --enable app_dumpchan --enable app_fax --enable app_externalivr --enable app_exec --enable app_echo --enable app_flash --enable app_followme --enable app_originate --enable app_mixmonitor --enable app_milliwatt --enable app_jack --enable app_meetme --enable app_page --enable app_playback --enable app_playtones --enable app_read --enable app_readexten --enable app_record --enable app_sayunixtime --enable app_senddtmf --enable app_skel --enable app_speech_utils --enable app_softhangup --enable app_talkdetect --enable app_system --enable app_transfer --enable app_userevent --enable app_verbose --enable app_waitforsilence --enable app_waituntil --enable app_while --enable cdr_adaptive_odbc --enable cdr_manager --enable cdr_custom --enable cdr_pgsql --enable cdr_syslog --enable cel_pgsql --enable cel_odbc --enable cel_manager --enable cel_custom --enable cel_sqlite3_custom --enable chan_alsa --enable chan_oss --enable chan_iax2 --enable chan_dahdi --enable codec_a_mu --enable codec_g722 --enable codec_gsm --enable format_g729 --enable format_vox --enable format_wav --enable format_siren14 --enable func_blacklist --enable func_aes --enable func_cdr --enable func_callerid --enable func_aes --enable func_iconv --enable func_global --enable func_extstate --enable func_env --enable func_enum --enable func_dialplan --enable func_dialgroup --enable pbx_dundi --enable pbx_config --enable pbx_lua --enable pbx_realtime --enable pbx_spool --enable res_config_mysql menuselect.makeopts
 
     #make commands
     commands=("make" "make install" "make samples" "make config" "ldconfig")
@@ -78,8 +79,7 @@ function installAsterisk() {
     #configure /etc/default/asterisk and asterisk.conf
     sed '/#AST_USER="asterisk"/s/^#//' -i /etc/default/asterisk 
     sed '/#AST_GROUP="asterisk"/s/^#//' -i /etc/default/asterisk 
-    sed '/runuser = asterisk ; The user to run as./s/^#//' -i /etc/asterisk/asterisk.conf 
-    sed '/rungroup = asterisk ; The group to run as./s/^#//' -i /etc/asterisk/asterisk.conf 
+    sed -i '/^;run\(user\|group\) =/ s/^;//' /etc/asterisk/asterisk.conf
 
     #restart asterisk daemon
     systemctl enable asterisk 
@@ -109,13 +109,14 @@ function installApache() {
 function installFreePBX() {
     #download FreePBX 16 archive
     cd /usr/src/
-    wget http://mirror.freepbx.org/modules/packages/freepbx/7.4/freepbx-16.0-latest.tgz
+    wget http://mirror.freepbx.org/modules/packages/freepbx/7.4/freepbx-16.0-latest.tgz 
     tar xzf ./freepbx-16.0-latest.tgz 
     cd /usr/src/freepbx/
+    systemctl stop asterisk 
     ./start_asterisk start 
     ./install -n 
     fwconsole ma disablerepo commercial 
-    fwconsole ma installall 
+    fwconsole ma install all 
     fwconsole ma delete firewall 
     fwconsole reload 
     fwconsole restart 
@@ -128,7 +129,7 @@ function installFreePBX() {
 function configureNetwork() {
     # Enable ufw
     yes | ufw enable
-    ufw allow 10000:20000
+    ufw allow 16384:32767/udp
     ufw allow 5060
     ufw allow 5061
     ufw allow ssh
@@ -138,7 +139,7 @@ function configureNetwork() {
 
     #install and configure fail2ban
     apt update
-    apt -y install fail2ban 
+    apt install -y fail2ban 
     cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
     sed -i 's/\/var\/log\/asterisk\/messages/\/var\/log\/asterisk\/full/g' /etc/fail2ban/jail.local
     sed -i '/\[asterisk\]/ { n; s/.*/enabled = true/ }' /etc/fail2ban/jail.local
